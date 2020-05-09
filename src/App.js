5bd0557db854aba4835e5a34766a45e736e1ab0e
klebedenko <kirilllebedenko@gmail.com>
2020-05-09 21:25:28 +0300

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Components/Blocks/Header/Header.js';
 import Footer from './Components/Blocks/Footer/Footer.js';
-import Service from './Components/Blocks/Service/Service.js';
+import ServiceBlock from './Components/Blocks/Service/Service.js';
 import Benefits from './Components/Blocks/Benefits/Benefits.js';
 import Clients from './Components/Blocks/Clients/Clients.js';
 import Achivments from './Components/Blocks/Achivments/Achivments.js';
@@ -10,20 +10,32 @@ import Review from './Components/Blocks/Review/Review.js';
 import './assets/styles/App.css';
 import Subscribe from './Components/Blocks/Subscribe/Subscribe';
 
+import About from './Components/Pages/About';
+import Service from './Components/Pages/Service';
+import Work from './Components/Pages/Work';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 
 const App = () => {
   return (
-    <>    
-      <Header />
-      <Service /> 
-      <Benefits /> 
-      <Clients />
-      <Achivments />
-      <Review />
-      <Subscribe />
-      <Footer />
-   </>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Header />
+          <ServiceBlock />
+          <Benefits />
+          <Clients />
+          <Achivments />
+          <Review />
+          <Subscribe />
+          <Footer />
+        </Route>
+        <Route path='/about' component = {About} />
+        <Route path='/service' component = {Service} />
+        <Route path='/work' component = {Work} />
+      </Switch>
+    </Router>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import About from './Components/Pages/About';
-import Service from './Components/Pages/Service';
-import Work from './Components/Pages/Work';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(
-    <Router>
-        <Switch>       
-        <Route path='/' exact component = {App} />
-        <Route path='/about' component = {About} />
-        <Route path='/service' component = {Service} />
-        <Route path='/work' component = {Work} /> 
-       </Switch>
-    </Router>,
-     document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
